7af434a1d410426d30a59658112fa33f6fd252fe
Niwedita Jaysawal <niweditaj@chetu.com>
2016-05-17 22:25:36 +0530

    deleted unused file

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -29,6 +29,7 @@ class AppointmentController extends Controller {
      *
      * @return \Illuminate\Http\Response
      */
+    
     public function index($id = null) {
         $patients = User::where('role', $this->patient_role)->get(['id', 'first_name', 'last_name']);
         $doctors = User::where('role', $this->doctor_role)->get(['id', 'first_name', 'last_name']);
