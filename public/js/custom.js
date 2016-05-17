051ae15b5e00c99390271cbc58936c6377c2658f
Amir Hanga <amirh@chetu.com>
2016-05-17 17:10:53 +0530

    Resolve bug where only one rows is shown in agendaweek view of doctor schedular calender

--- public/js/custom.js
+++ public/js/custom.js
@@ -16,9 +16,9 @@ $('#external-events div.external-event').each(function() {
 var eventObject = {
 title: $.trim($(this).text()) // use the element's text as the event title
 };
-        // store the Event Object in the DOM element so we can get to it later
-        $(this).data('eventObject', eventObject);
-        // make the event draggable using jQuery UI
+	        // store the Event Object in the DOM element so we can get to it later
+	        $(this).data('eventObject', eventObject);
+   // make the event draggable using jQuery UI
         $(this).draggable({
 zIndex: 999,
         revert: true, // will cause the event to go back to its
@@ -61,23 +61,27 @@ zIndex: 999,
                         'firstDay':moment().format('MM/DD/YYYY'),
                         droppable: false, // this allows things to be dropped onto the calendar !!!
                         drop: function(date, allDay) { // this function is called when something is dropped
-                        var $externalEvent = $(this);
-                                // retrieve the dropped element's stored Event Object
-                                var originalEventObject = $externalEvent.data('eventObject');
-                                // we need to copy it, so that multiple events don't have a reference to the same object
-                                var copiedEventObject = $.extend({}, originalEventObject);
-                                // assign it the date that was reported
-                                copiedEventObject.start = date;
-                                copiedEventObject.allDay = allDay;
-                                copiedEventObject.className = $externalEvent.attr('data-event-class');
-                                // render the event on the calendar
-                                // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-                                $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-                                // is the "remove after drop" checkbox checked?
-                                if ($('#RemoveAfterDrop').is(':checked')) {
-                        // if so, remove the element from the "Draggable Events" list
-                        $(this).remove();
-                        }
+                        	var $externalEvent = $(this);
+                            // retrieve the dropped element's stored Event Object
+                            var originalEventObject = $externalEvent.data('eventObject');
+                            
+							// we need to copy it, so that multiple events don't have a reference to the same object
+                            var copiedEventObject = $.extend({}, originalEventObject);
+
+                            // assign it the date that was reported
+                            copiedEventObject.start = date;
+                            copiedEventObject.allDay = allDay;
+                            copiedEventObject.className = $externalEvent.attr('data-event-class');
+
+                            // render the event on the calendar
+                            // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                            $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+
+                            // is the "remove after drop" checkbox checked?
+                            if ($('#RemoveAfterDrop').is(':checked')) {
+                        		// if so, remove the element from the "Draggable Events" list
+                        		$(this).remove();
+                       		 }
 
                         },
                         events: events,
@@ -112,6 +116,7 @@ zIndex: 999,
                 $calendarButtons
                 .attr({'class': 'btn btn-sm btn-default'});
         };
+
         var initDoctorSchedulrCalendar = function(events, inputDate = null, slotMinutes = 30, start = '00:00:00', end = '24:00:00') {
         var $calendar = $('#calendar');
                 var date = (inputDate == null || inputDate == "" || inputDate == undefined)? new Date() : new Date(inputDate);
@@ -130,8 +135,8 @@ zIndex: 999,
                         },
                         allDaySlot: false,
                         slotMinutes: slotMinutes,
-                        minTime: "00:09:00",
-                        maxTime: "00:17:00",
+                        minTime: start,
+                        maxTime: end,
                         timeFormat: 'h:mm A',
                         titleFormat: {
                         month: 'MMMM YYYY', // September 2009
