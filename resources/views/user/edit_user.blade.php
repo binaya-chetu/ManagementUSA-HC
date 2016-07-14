4f805b47924fc10d0ec69998001a7706274e14be
Suresh Chaurasia <sureshc@chetu.com>
2016-07-14 23:20:57 +0530

    add form id in edituser file

--- resources/views/user/edit_user.blade.php
+++ resources/views/user/edit_user.blade.php
@@ -21,7 +21,7 @@
                     </div>
                     <h2 class="panel-title"><strong>Add User </strong> </h2>
                 </header>
-                {!! Form::model($user, ['method' => 'post','url' => ['/user/updatedUser', $user->id], 'id' => 'user']) !!}
+                {!! Form::model($user, ['method' => 'post','url' => ['/user/updatedUser', $user->id], 'id' => 'addUser']) !!}
                 {{ csrf_field() }}
                 <div class="panel-body">
                     <div class="row">
