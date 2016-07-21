145bca01a5c5d7804a242fe51bd269afb31c5e4f
Vipul Kumar <vipulk@chetu.com>
2016-07-21 16:22:49 +0530

    comment the appointment in the patient listing

--- resources/views/patient/patients.blade.php
+++ resources/views/patient/patients.blade.php
@@ -59,10 +59,10 @@
                         <td class="actions">
                             <a href="/patient/edit/{{ base64_encode($patient->id) }}"  title="Edit"><i class="fa fa-pencil"></i></a> | 
                            <a data-href="/patient/delete/{{ base64_encode($patient->id) }}" href="javascrpt:void(0)" class="on-default remove-row confirmation-callback" ><i class="fa fa-trash-o"></i></a> 
-                           @if(!($patient['patientDetail']->never_treat_status)) 
+<!--                           @if(!($patient['patientDetail']->never_treat_status)) 
                            |
                            <a href="/appointment/newAppointment/{{ base64_encode($patient->id) }}" class="on-default" title="Add Appointment"><i class="fa fa-calendar"></i></a>
-                           @endif
+                           @endif-->
                         </td>
                     </tr>
                     @endforeach 
