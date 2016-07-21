bcc8d74a9c61858f14163c501b31ea33be52bc3a
Vipul Kumar <vipulk@chetu.com>
2016-07-21 17:41:11 +0530

    add the patient status regarding the sale - Current or New

--- resources/views/appointment/appointment_after_report_popup.blade.php
+++ resources/views/appointment/appointment_after_report_popup.blade.php
@@ -29,6 +29,13 @@
                     {{ Form::text('last_name', old('last_name'), ['class' => 'form-control', 'id' => 'last_name', 'placeholder' => 'Last Name']) }}                    
                 </div>
             </div>
+            <div class="form-group">
+                {{ Form::label('patient_status', 'Patient Status', array('class' => 'col-sm-3 control-label')) }}
+                <div class="col-md-8">
+                    <?php $patientStatus = patientStatus(); ?>
+                    {{ Form::select('patient_status', ['' => 'Select the Patient Status'] + $patientStatus , null, ['class' => 'form-control', 'id' => 'patientSaleStatus', 'disabled' => 'disabled']) }}
+                </div>
+            </div>
 
             <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}" id='emailParent'>
                 {{ Form::label('email', 'Email', array('class' => 'col-sm-3 control-label')) }}
