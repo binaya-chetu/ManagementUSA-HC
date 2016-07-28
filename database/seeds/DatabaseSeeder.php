08f7f565eeef914ab9f26ad553e67e2c4cb8ae67
Vipul Kumar <vipulk@chetu.com>
2016-07-28 16:01:33 +0530

    Call all the seeder in the main seeder file

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,9 +11,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
          $this->call(AppointmentSourcesTableSeeder::class);
+         $this->call(CategoriesTableSeeder::class);
          $this->call(CategoryAdOnsSeeder::class);
          $this->call(CategoryTypesSeeder::class);
          $this->call(FollowupStatusTableSeeder::class);
+         $this->call(InvoiceTableSeeder::class);
+         $this->call(OrderDetailsTableSeeder::class);
+         $this->call(OrderTableSeeder::class);
+         $this->call(PaymentTableSeeder::class);
          $this->call(PermissionsTableSeeder::class);
          $this->call(ReasonCodesTableSeeder::class);
          $this->call(RolesTableSeeder::class);
