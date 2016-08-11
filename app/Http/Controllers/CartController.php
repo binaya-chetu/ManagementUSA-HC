a2c69b4324b377e30ff2961e5d89e04691086657
Amir Hanga <amirh@chetu.com>
2016-08-11 17:49:19 +0530

    refactor code in CartController and move the large orm queries to modal

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -82,57 +82,16 @@ class CartController extends Controller
      */
     public function showCart($id){
 		$patientId = base64_decode($id);
-		
-		$category_list = [];
-		$category_detail_list = [];
-		
-		$original_package_price = [];
-		$discouonted_package_price = [];
-		$package_discount = [];		
+		$cart = Cart::getCartDetails($patientId);
 
-        $cart = Cart::with('patient', 'user', 'categoryTypes', 'categories', 'categories.packages', 'categories.packages.Product')->where('patient_id', $patientId)->get();
-		
-		foreach($cart as $i => $v){
-			
-			$original_package_price[$v->id] = 0;
-			$discouonted_package_price[$v->id] = 0;
-			
-			$category_list[$v->id] = [
-								'category_id' => $v->category_id, 
-								'category' => $v->categories->cat_name, 
-								'duration' => $v->categories->duration_months, 
-								'user_id' => $v->user_id, 
-								'user' => $v->user->first_name.' '.$v->user->last_name, 
-								'patient_id' => $v->patient_id, 
-								'patient' => $v->patient->first_name.' '.$v->patient->last_name,
-								'category_type_id' => $v->category_type_id,
-								'category_type' => $v->categoryTypes->name
-							]; 
-
-			$category_detail_list[$v->id] = [];
-			foreach($v->categories->packages as $ind => $val){
-				$category_detail_list[$v->id][$ind]['product_id'] = $val->product_id;
-				$category_detail_list[$v->id][$ind]['sku'] = $val->product->sku;
-				$category_detail_list[$v->id][$ind]['product'] = $val->product->name;
-				$category_detail_list[$v->id][$ind]['count'] = $val->product_count;
-				$category_detail_list[$v->id][$ind]['discount_price'] = $val->product_price;
-				$category_detail_list[$v->id][$ind]['original_price'] = $val->product->price;
-				$category_detail_list[$v->id][$ind]['unit_of_measurement'] = $val->product->unit_of_measurement;
-				
-				$original_package_price[$v->id] +=  $val->product_count * $val->product->price;
-				$discouonted_package_price[$v->id] +=  $val->product_price;
-			}
-			$package_discount[$v->id] =  $original_package_price[$v->id] - $discouonted_package_price[$v->id];
-		}	
-		
-        return view('cart.cart',['category_list' => $category_list, 'category_detail_list' => $category_detail_list, 'original_package_price' => $original_package_price, 'discouonted_package_price' => $discouonted_package_price, 'package_discount' => $package_discount]);
+        return view('cart.cart',['category_list' => $cart['category_list'], 'category_detail_list' => $cart['category_detail_list'], 'original_package_price' => $cart['original_package_price'], 'discouonted_package_price' => $cart['discouonted_package_price'], 'package_discount' => $cart['package_discount']]);
     }
 	
     /**
      * Remove an item from cart
      *
      * @return \resource\view\cart\cart
-     */
+    */
     public function removeItem($id){
 		$id = base64_decode($id);
         Cart::destroy($id);
