34667e0852d12b75884ddd7ca1b2a0f8b94d65d0
Niwedita Jaysawal <niweditaj@chetu.com>
2016-08-18 19:05:17 +0530

    inventory layout page

--- resources/views/appointment/listappointment.blade.php
+++ resources/views/appointment/listappointment.blade.php
@@ -107,7 +107,8 @@
                                         break;
                                 }
                                 ?>
-                            </div></td>
+                            </div>
+                          </td>
                         <td class="actions">
                             <a href="#" class="hidden on-editing save-row"><i class="fa fa-save"></i></a>
                             <a href="#" class="hidden on-editing cancel-row"><i class="fa fa-times"></i></a>
