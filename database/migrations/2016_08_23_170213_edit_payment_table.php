21562a648343f22565cc7fa8710311900f0d3045
Vipul Kumar <vipulk@chetu.com>
2016-08-23 17:46:50 +0530

    Change in the editorderdetail & editpayment migration

--- database/migrations/2016_08_23_170213_edit_payment_table.php
+++ database/migrations/2016_08_23_170213_edit_payment_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class EditOrderDetailTable extends Migration
+class EditPaymentTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,7 +12,12 @@ class EditOrderDetailTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('payments', function($table)
+        {
+            $table->dropColumn(['user_id', 'order_id', 'amount']);     
+            $table->integer('patient_id')->after('id');
+            $table->integer('agent_id')->after('patient_id');            
+	});        
     }
 
     /**
