cc23d7928be88d6427721e32eac073cc1fa7a60b
Vipul Kumar <vipulk@chetu.com>
2016-08-23 15:13:25 +0530

    Make the migration file

---
+++ database/migrations/2016_08_22_220918_edit_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EditOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function($table)
+        {
+            $table->dropColumn(['agent_id', 'user_id', 'subtotal_amount', 'discount_amount', 'total_amount', 'package_id', 'package_type']);     
+            $table->integer('payment_id')->after('id');
+            $table->string('category')->after('payment_id');            
+            
+            $table->decimal('price',10,2)->after('package_type');
+	});
+        Schema::table('orders', function($table)
+        {
+            $table->string('package_type')->after('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
