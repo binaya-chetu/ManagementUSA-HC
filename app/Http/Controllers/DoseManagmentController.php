0f85c6959f0e370d205d6d4d5e5d6bd68b7d156f
Suresh Chaurasia <sureshc@chetu.com>
2016-09-05 23:26:28 +0530

    implements the functionality to save doses information

--- app/Http/Controllers/DoseManagmentController.php
+++ app/Http/Controllers/DoseManagmentController.php
@@ -12,8 +12,11 @@ use Illuminate\Config\Repository;
 use View;
 use App;
 use DB;
+use Auth;
 use Exception;
 use App\User;
+use App\Patient;
+use App\TrimixDoses;
 
 class DoseManagmentController extends Controller
 {
@@ -27,6 +30,7 @@ class DoseManagmentController extends Controller
     public function __construct() {
         $this->middleware('auth');
     }
+    
     /**
      * Display a listing of the resource.
      *
@@ -46,9 +50,63 @@ class DoseManagmentController extends Controller
    
     
     public function getPatientDetails(Request $request) {
-        $patientData = User::with('patientDetail')->where('id',$request->patient_id)->get();
+        $patientData = User::with('patientDetail', 'trimixDoses')->where('id',$request->patient_id)->first();
         return $patientData;
-        die;
+        exit();
+    }
+    
+    /**
+     * This function is used to save the new role in tables.
+     *
+     * @param Request
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        
+        // define validation rule
+        $this->validate($request, [
+            'doctor' => 'required',
+            'amount1' => 'required',
+            'medicationA1' => 'required',
+            'amount2' => 'required',
+            'medicationA2' => 'required',
+            'amount3' => 'required',
+            'medicationB1' => 'required',
+            'amount4' => 'required',
+            'medicationB2' => 'required'
+        ]);
+        
+        //echo "<pre>";print_r($request->all());die;
+        // create User object
+        $trimixData = new TrimixDoses;
+        
+        $trimixData->patient_id = $request->patient_id;
+        $trimixData->agent_id = Auth::user()->id;
+        $trimixData->dose_type = 'A';
+        $trimixData->quantity = (isset($request->quantity)) ? $request->quantity : 1;
+        $trimixData->doctor = $request->doctor;
+        $trimixData->amount1 = $request->amount1;
+        $trimixData->medicationA1 = $request->medicationA1;
+        $trimixData->amount2 = $request->amount2;
+        $trimixData->medicationA2 = $request->medicationA2;
+        $trimixData->amount3 = $request->amount3;
+        $trimixData->medicationB1 = $request->medicationB1;
+        $trimixData->amount4 = $request->amount4;
+        $trimixData->medicationB2 = $request->medicationB2;
+        $trimixData->antidote = (isset($request->antidote)) ? $request->antidote : '';
+        $trimixData->dose_start_date = date('Y-m-d h:i:s');
+        
+        // save user data in user table
+        if ($trimixData->save()) {
+            // set the flash message.
+            \Session::flash('flash_message', 'Doses saved successfully.');
+            return redirect('/doses/doseManagement');
+        } else {
+            \Session::flash('flash_message', 'There are something went wrong Plz Try again.');
+            return Redirect::back();
+        }
     }
 
 }
