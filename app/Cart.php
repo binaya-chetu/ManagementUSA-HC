18635d1903c7775f41bfee7a8d3b8e20042d1101
Amir Hanga <amirh@chetu.com>
2016-09-17 01:52:40 +0530

    Resolved issue with cart product list aand price

--- app/Cart.php
+++ app/Cart.php
@@ -19,10 +19,11 @@ class Cart extends Model
 		
 		$cart = Cart::with('patient', 'user', 'categoryTypes', 'categories', 'categories.packages', 'categories.packages.Product')->where('patient_id', $patientId)->get();	
 
+
 		foreach($cart as $i => $v){
 			$original_package_price[$v->id] = 0;
 			$discouonted_package_price[$v->id] = 0;
-			
+		
 			$category_list[$v->id] = [
 								'category_id' => $v->category_id, 
 								'category' => $v->categories->cat_name, 
@@ -37,22 +38,24 @@ class Cart extends Model
 
 			$category_detail_list[$v->id] = [];
 			foreach($v->categories->packages as $ind => $val){
-				$category_detail_list[$v->id][$ind]['package_id'] = $val->id;
-				$category_detail_list[$v->id][$ind]['product_id'] = $val->product_id;
-				$category_detail_list[$v->id][$ind]['sku'] = $val->product->sku;
-				$category_detail_list[$v->id][$ind]['product'] = $val->product->name;
-				$category_detail_list[$v->id][$ind]['count'] = $val->product_count;
-				$category_detail_list[$v->id][$ind]['discount_price'] = $val->product_price;
-				$category_detail_list[$v->id][$ind]['original_price'] = $val->product->price;
-				$category_detail_list[$v->id][$ind]['unit_of_measurement'] = $val->product->unit_of_measurement;
-				
-				$original_package_price[$v->id] +=  $val->product_count * $val->product->price;
-				$discouonted_package_price[$v->id] +=  $val->product_price;
-				$total_cart_price +=  $val->product_price;
+ 				if($category_list[$v->id]['category_type_id'] == $val['category_type']){
+					$category_detail_list[$v->id][$ind]['package_id'] = $val->id;
+					$category_detail_list[$v->id][$ind]['product_id'] = $val->product_id;
+					$category_detail_list[$v->id][$ind]['sku'] = $val->product->sku;
+					$category_detail_list[$v->id][$ind]['product'] = $val->product->name;
+					$category_detail_list[$v->id][$ind]['count'] = $val->product_count;
+					$category_detail_list[$v->id][$ind]['discount_price'] = $val->product_price;
+					$category_detail_list[$v->id][$ind]['original_price'] = $val->product->price;
+					$category_detail_list[$v->id][$ind]['unit_of_measurement'] = $val->product->unit_of_measurement;
+					
+					$original_package_price[$v->id] +=  $val->product_count * $val->product->price;
+					$discouonted_package_price[$v->id] +=  $val->product_price;
+					$total_cart_price +=  $val->product_price; 
+				}
 			}
 			$package_discount[$v->id] =  $original_package_price[$v->id] - $discouonted_package_price[$v->id];
-		}			
-		
+		} 
+	
 		return ['category_list' => $category_list, 'category_detail_list' => $category_detail_list, 'original_package_price' => $original_package_price, 'discouonted_package_price' => $discouonted_package_price,	'package_discount' => $package_discount, 'total_cart_price' => $total_cart_price];
 	}
 	
